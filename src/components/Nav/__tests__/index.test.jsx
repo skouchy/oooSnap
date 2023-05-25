49a984114c1083bea3122662623e7f36af5ec667
skouch <skouchy24@gmail.com>
2023-05-25 04:42:48 -0700

    feat(conditional-rendering): Categories array for 'lift-up state' moved to App.js while useEffect conditionally renders style effect on Nav tabs based onClick

--- src/components/Nav/__tests__/index.test.jsx
+++ src/components/Nav/__tests__/index.test.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import React from "react";
-import { render, cleanup, screen, getByTestId } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from "..";
 
