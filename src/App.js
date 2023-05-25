f112e0cbbb4b337a97dd4b8377c6c51db4d1ec9b
skouch <skouchy24@gmail.com>
2023-05-25 01:40:13 -0700

    About Me page components + Navbar components added & imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav'
 import About from './components/About';
 
 function App() {
   return (
     <div>
+        <Nav></Nav>
       <main>
         <About></About>
       </main>
