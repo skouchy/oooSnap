3b76bd1ca3d5a4de8d8c0c0f82d1572b6abea3da
skouch <skouchy24@gmail.com>
2023-05-25 03:15:14 -0700

    feat(tests): Nav component tests run and passed

---
+++ src/components/Nav/__tests__/index.test.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, cleanup, screen, getByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from "..";
+
+describe('Nav component', () => {
+    // baseline test
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        // Arrange
+        const { getByLabelText } = render(<Nav />);
+        // Assert
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+});
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        // Arrange
+        const { getByTestId } = render(<Nav />);
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
+        // Assert
+    })
+})
+
+afterEach(cleanup);
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -27,7 +27,7 @@ function Nav() {
   return (
     <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
             📸
           </span>
@@ -37,7 +37,7 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About Me</a>
+            <a data-testid="about" href="#about">About Me</a>
           </li>
           <li>
             <span>Contact</span>
